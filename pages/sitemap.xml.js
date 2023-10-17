e8bd537a2d248ddfdb7d9ac2bdd02c4d19215112
permlap-phola <permlap@tatugacamp.com>
2023-10-17 22:31:16 +0700

    dynamic sitemap for SEO

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,14 +1,33 @@
-function SiteMap() {
+function generateSiteMap({ host }) {
+  console.log(host);
   return `<?xml version="1.0" encoding="UTF-8"?>
-     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-       <!--We manually set the two URLs we know already-->
-       <url>
-         <loc>mellifluous-selkie-e951db.netlify.app/</loc>
-       </url>
-       <url>
-         <loc>mellifluous-selkie-e951db.netlify.app/welcome</loc>
-       </url>
-     </urlset>
-   `;
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <!--We manually set the two URLs we know already-->
+    <url>
+      <loc>${host}/</loc>
+    </url>
+    <url>
+      <loc>${host}/welcome</loc>
+    </url>
+  </urlset>
+`;
+}
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
 }
 export default SiteMap;
+
+export async function getServerSideProps({ res, req }) {
+  const host = req.headers.host;
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap({ host });
+
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+  return {
+    props: {},
+  };
+}
