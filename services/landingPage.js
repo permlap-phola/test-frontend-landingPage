5fcd3c49adcf0aee28f3cf37a34b4c2e93083501
permlap-phola <permlap@tatugacamp.com>
2023-12-05 19:12:03 +0700

    Pass language user's browser to serverSide

--- services/landingPage.js
+++ services/landingPage.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 import Error from "next/error";
 
-export async function GetLandingPageService({ host }) {
+export async function GetLandingPageService({ host, language }) {
   try {
     const landingPage = await axios.get(
       `https://backend-landingpage-admin-dasboard-n2vkrqhb2a-uc.a.run.app/public/landing-page/get`,
       {
         params: {
           domain: host,
+          language,
+        },
+        headers: {
+          "Content-Type": "application/json",
         },
       }
     );
