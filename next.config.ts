bd0df94445f993373e7378e5216b9eaa66866d29
permlap-phola <permlap@tatugacamp.com>
2024-10-31 22:50:00 +0700

    Remove Header Next config

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,8 @@
 import { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const cspHeader = `
- script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com 'unsafe-eval';
-`;
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
-  },
 
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
